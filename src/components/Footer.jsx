a3fdd78893612f22f3c7ec300272e4c5f6aaced8
jaina0831 <103718281+jaina0831@users.noreply.github.com>
2023-04-03 04:01:50 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,18 @@ export default function Footer(){
     <div className="footer__above py-5">
       <div className="container">
         <div className="row my-3">
-          <ul className="footer__list col-lg-4">
-            <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-facebook-f"></i></a></li>
+          <div className="first-icon-wrap">
+          <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-facebook-f"></i></a></li>
             <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-google-plus-g"></i></a></li>
             <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-linkedin-in"></i></a></li>
-          </ul>
+          </div>
         <h5 className="footer__description col-lg-8">
         使用條款 隱私權政策 客服中心
         </h5>
         </div>
         </div>
       </div>
+
 
     <div className="footer__below">
       <div className="container">
