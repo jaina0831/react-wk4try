29e3eadc3e7203bdfdeba16508696dc1890636a5
jaina0831 <103718281+jaina0831@users.noreply.github.com>
2023-04-03 03:43:35 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,17 @@ export default function Footer(){
     <div className="footer__above py-5">
       <div className="container">
         <div className="row my-3">
-          <h5 className="footer__description col-lg-8">
-            臺北市和平東路二段134號，國立臺北教育大學 數位科技設計學系
-          </h5>
           <ul className="footer__list col-lg-4">
             <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-facebook-f"></i></a></li>
             <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-google-plus-g"></i></a></li>
             <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-linkedin-in"></i></a></li>
           </ul>
+        <h5 className="footer__description col-lg-8">
+        使用條款 隱私權政策 客服中心
+        </h5>
+        </div>
         </div>
       </div>
-    </div>
 
     <div className="footer__below">
       <div className="container">
