76ea08722cd868450a0e6b951c52c98a44db3acc
jaina0831 <103718281+jaina0831@users.noreply.github.com>
2023-04-04 15:13:53 +0800

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,56 +1,33 @@
 import { Row, Col } from "antd";
 import images from "../json/image.json";
-export default function ProductList() {
-    return (
-        <div>
-            <article className="image__layout py-3 py-sm-5">
-                <div className="container">
-                    <h1 className="text-center">IMAGES</h1>
-                    <hr className="divider--dark" />
-                    
-                        <Row gutter={[32, 32]}>
-                            {images.map(image => (
-                                <Col key={image.id}
-                                sm={{span:12}}
-                                lg={{span:8}}
-                                xl={{span:6}}
-                                >
-                                        <div>
-                                            <a href="" className="image__link">
-                                                <img src={image.link} alt="" className="image__style" />
-                                            </a>
-                                            <p className="image_title">{image.name}</p>
-                                        </div>
-                                        </Col>
-                                    ))}
-                                
-                                
-                        </Row>
-
-
-
-                   
-                </div>
-            </article>
-
-            <article className="description__layout py-4 py-sm-5">
-                <div className="container d-flex flex-column align-items-center">
-                    <h1>DESCRIPTIONS</h1>
-                    <hr className="divider--light" />
-                    <p className="text-justify description__content py-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum
-                        corrupti ut iure aliquam. Quod, perferendis. Libero tempore iste, animi sint numquam illum quasi laboriosam
-                        unde pariatur quidem! Suscipit consequatur nihil dolor impedit temporibus ad cum, voluptatum odit ratione
-                        ullam
-                        eligendi amet beatae aliquid eaque nesciunt optio nobis ducimus itaque. Voluptatibus aspernatur assumenda
-                        quod
-                        quo error consequuntur, vero iusto reprehenderit repellat maiores, sapiente dolores voluptates minima. Alias
-                        dolore assumenda fuga quae repellat voluptatum quo, saepe exercitationem perspiciatis reiciendis modi
-                        tempora.
-                        Recusandae explicabo iusto corporis nam numquam dignissimos, necessitatibus, iure non culpa repellat
-                        repudiandae temporibus! Saepe excepturi tempore iusto eos sit!</p>
-                </div>
-            </article>
-        </div>
-
-    );
-}
+import { Avatar, List } from 'antd';
+const data = [
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+];
+const App = () => (
+  <List
+    itemLayout="horizontal"
+    dataSource={data}
+    renderItem={(item, index) => (
+      <List.Item>
+        <List.Item.Meta
+          avatar={<Avatar src={`https://joesch.moe/api/v1/random?key=${index}`} />}
+          title={<a href="https://ant.design">{item.title}</a>}
+          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+        />
+      </List.Item>
+    )}
+  />
+);
+export default App;
