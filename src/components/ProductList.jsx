2bf4dea10349305555bbdec028ad46edc5df7dba
jaina0831 <103718281+jaina0831@users.noreply.github.com>
2023-04-04 15:47:00 +0800

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,33 +1,37 @@
 import { Row, Col } from "antd";
 import images from "../json/image.json";
 import { Avatar, List } from 'antd';
-const data = [
-  {
-    title: 'Ant Design Title 1',
-  },
-  {
-    title: 'Ant Design Title 2',
-  },
-  {
-    title: 'Ant Design Title 3',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-];
-const App = () => (
-  <List
-    itemLayout="horizontal"
-    dataSource={data}
-    renderItem={(item, index) => (
-      <List.Item>
-        <List.Item.Meta
-          avatar={<Avatar src={`https://joesch.moe/api/v1/random?key=${index}`} />}
-          title={<a href="https://ant.design">{item.title}</a>}
-          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-        />
-      </List.Item>
-    )}
-  />
+export default function ProductList() {
+// const data = [
+//   {
+//     title: 'Ant Design Title 1',
+//   },
+//   {
+//     title: 'Ant Design Title 2',
+//   },
+//   {
+//     title: 'Ant Design Title 3',
+//   },
+//   {
+//     title: 'Ant Design Title 4',
+//   },
+// ];
+// const App = () => (
+//   <List
+//     itemLayout="horizontal"
+//     dataSource={data}
+//     renderItem={(item, index) => (
+//       <List.Item>
+//         <List.Item.Meta
+//           avatar={<Avatar src={`https://joesch.moe/api/v1/random?key=${index}`} />}
+//           title={<a href="https://ant.design">{item.title}</a>}
+//           description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+//         />
+//       </List.Item>
+//     )}
+//   />
+// );
+return (
+1111
 );
-export default App;
+}
